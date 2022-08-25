69f161ea8fd0f169e49b756ee209806d0b19bf98
Manipriyan <manipriyan@6storage.com>
2022-08-25 12:18:40 +0530

    removed the store

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Provider } from "react-redux";
-import Store from "./Components/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={Store}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
